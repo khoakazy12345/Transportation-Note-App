5c1af8f350ef1493fc6c18d4871bd2309ca6c9cc
Jose Salinas <j.jr.2011@outlook.com>
2020-11-13 20:58:42 -0500

    Added to localhost:8080

--- serv/server.js
+++ serv/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const site = express();
 
+site.get('/',(req,res) => {
+    res.send('HELLO');
+});
 
 site.listen(8080, () => {
     console.log('Listening on 8080');
