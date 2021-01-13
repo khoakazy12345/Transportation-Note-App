97712a6966e7ac5cd654210250ffc5049aceb8e5
Khoa Nguyen <nguyenk7@miamioh.edu>
2021-01-13 00:15:05 -0500

    CSS Semi Final Commit

---
+++ my-app/src/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import './Footer.css';
+
+class Header extends React.Component    {
+    
+    render()    {
+        return  (
+            <div>
+                <h1>The end</h1>
+                <h2>Made by Team CrawFish</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header.js';
-import Navbar from './Navbar.js';
 import Map from './Map.js';
+import Footer from './Footer.js';
 import './index.css';
 
 class App extends React.Component {
 	render() {
     	return (
       	<body>
-		  	{/* <div className="NavBar">
-			  <Navbar />
-			</div> */}
-
         	<div className="Header">
 				<Header />
         	</div>
 
 			<div className="Google-Map">
 				<Map/>
+			</div>
+
+			<div className="Footer">
+				<Footer/>
 			</div>
       	</body>
     	);
